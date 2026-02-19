cc43e0ce22967fa16edd5f400296facbb0d6497d
ZwePP <zwepy02@gmail.com>
2026-02-19 11:59:26 +0700

    Response streaming

--- app.py
+++ app.py
@@ -18,26 +18,31 @@ def gpt_response(user_prompt):
     input = [
         {"role": "system", 
          "content": """You are an assistant that have knowledge in IT field. \
-        Generate best responses as simple as possible. \
+        Generate    best responses as simple as possible. \
         If you can't find the resource or don't know the answer, be direct. 
         Avoid generating too many words in a sentence. Only keypoints """},
 
         {
             "role" : "user",
-            "content" : "What is the optimal temperature for computer operation?"
+            "content" : "What is the optimal humidity for computer operation?"
         },
         {
             "role": "assistant",
-            "content" : f"Optimal temperature range: 30% - 50%. - prevent static electricity -reduce risks of corrosion"
+            "content" : f"Optimal humidity range: 30% - 50%. - prevent static electricity -reduce risks of corrosion"
         },
 
 
         {"role": "user", 
          "content": user_prompt}
     ],
+    stream=True
     )
-    string_response = response.output[0].content[0].text
-    return string_response
+    
+    #Stream
+    for event in response:
+        if event.type == "response.output_text.delta":
+            print(event.delta, end="", flush=True)    
+        
 
 
 
@@ -46,5 +51,8 @@ if __name__ == "__main__":
     user_input = input("Input: ")
     while(user_input != 'q'):
         answer = gpt_response(user_input)
-        print(answer)
+        print("\n")
         user_input = input("Input (q to exit!): ")
+
+
+
